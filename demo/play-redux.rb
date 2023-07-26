84810b85eeb6a21fb3b78887c7bd0232165f63bd
Steffen Roller <steffenr@vineta.ca>
2023-07-26 14:05:15 -0400

    milestone

--- demo/play-redux.rb
+++ demo/play-redux.rb
@@ -4,7 +4,8 @@ require 'open3'
 require 'awesome_print'
 
 if Gem::Platform.local.to_s =~ /linux/
-  ENGINE_PATH = '/usr/games/stockfish'
+  # ENGINE_PATH = '/usr/games/stockfish'
+  ENGINE_PATH = 'stockfish'
 elsif Gem::Platform.local.to_s =~ /mingw32/
   ENGINE_PATH = 'c:/util/stockfish_14_x64_avx2.exe'
 end
@@ -15,7 +16,7 @@ def sec2human(seconds)
   hours = seconds / 3600
   minutes = (seconds - (hours*3600)) / 60
   secs = seconds % 60
-  sprintf "%02d:%02d:%02d", hours, minutes, secs
+  sprintf "%0d:%02d:%02d", hours, minutes, secs
 end
 
 def send_string(channel, msg)
@@ -26,29 +27,35 @@ end
 
 move = ""
 
-def uci_find_best_move(fen, depth)
-  # fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" if fen.nil?
+def uci_find_best_move(fen:, depth:, hash:, threads: )
+  fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" if fen.nil?  # standard position
 
   warn "start #{ENGINE_PATH}"
 
-  sin, sout, thread = Open3.popen2e(ENGINE_PATH)
+  uci_inp, uci_out, thread = Open3.popen2e(ENGINE_PATH)
 
   input_thread = nil
   output_thread = nil
   bestmove = ""
+  time = ""
   score = 0
+  engine_version = "unknown"
 
   output_thread = Thread.new do
     warn "start output thread"
     line = ""
     while true do
       
-      line = sout.gets.chomp
+      line = uci_out.readline.chomp
       # ap line
       case
+      when line[0..8] == 'Stockfish'
+        engine_version = line.match(/(Stockfish [.0-9]+)/)[1] # catch the version number eg 14.1
+        puts line
       when val = line.match(/depth (\d+) .*score cp (\d+) .*time (\d+).*pv (.*)$/)
         score = val[2].to_f / 100
-        printf "%.1f depth: %d %s (%s)\n", val[2].to_f/100, val[1].to_i, val[4], sec2human(val[3])
+        time = sec2human(val[3])
+        printf "%.2f depth: %d %s (%s)\n", val[2].to_f/100, val[1].to_i, val[4], time
       else
         puts "> #{line}" unless line =~ /currmove/
       end
@@ -62,18 +69,18 @@ def uci_find_best_move(fen, depth)
 
   input_thread = Thread.new do
     warn "start input thread"
-    send_string(sin, "uci")
-    send_string(sin, "isready")
-    send_string(sin, "ucinewgame")
-    send_string(sin, "setoption name UCI_Chess960 value true")
-    send_string(sin, "setoption name Threads value 6")
-    send_string(sin, "setoption name Hash value 4096")
-    send_string(sin, "setoption name MultPV value 2")
-    send_string(sin, "position fen #{fen}")
-    send_string(sin, "go depth #{depth}")
+    send_string(uci_inp, "uci")
+    send_string(uci_inp, "isready")
+    send_string(uci_inp, "ucinewgame")
+    send_string(uci_inp, "setoption name UCI_Chess960 value true")
+    send_string(uci_inp, "setoption name Threads value #{threads}")
+    send_string(uci_inp, "setoption name Hash value #{hash}")
+    send_string(uci_inp, "setoption name MultiPV value 1")
+    send_string(uci_inp, "position fen #{fen}")
+    send_string(uci_inp, "go depth #{depth}")
     while true do
       command = gets.chomp
-      send_string(sin, command)
+      send_string(uci_inp, command)
       if command == "quit"
         # set_global_stop true
         Thread.kill output_thread
@@ -85,20 +92,25 @@ def uci_find_best_move(fen, depth)
 
   input_thread.join
   output_thread.join
-  sin.close
-  sout.close
+  uci_inp.close
+  uci_out.close
   warn "UCI out"
-  return bestmove, score
+  return bestmove, score, engine_version, time
 end
 
 # main
 
-depth = 30
+# parameters should come from config file or command line
+depth = 35 
+hash_size = 4096 # in megabytes
+threads = 6
+
 File.open("starting_positions.txt", "r") do |f|
-  File.write("best_move.csv", "#fen,bestmove,score,depth #{Time.now} #{ENGINE_PATH}\n", mode: "a")
+  File.write("best_move.csv", "#parameters: depth #{depth}, hash #{hash_size}, threads #{threads}, start #{Time.now} with #{ENGINE_PATH}\n", mode: "a")
+  File.write("best_move.csv", "#headers: fen,bestmove,score,depth,version,time\n", mode: "a")
   while fen = f.readline.chomp do
-    move, score = uci_find_best_move(fen, depth)
-    File.write("best_move.csv", "#{fen},#{move},#{"%.2f" % score},#{depth}\n", mode: "a")
+    move, score, version, time = uci_find_best_move(fen: fen, depth: depth, hash: hash_size, threads: threads)
+    File.write("best_move.csv", "#{fen},#{move},#{"%.2f" % score},#{depth},#{version},#{time}\n", mode: "a")
   end
 end
 warn "done"
